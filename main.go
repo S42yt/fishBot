828fb6448bc1d8f0459b8dd2c42d2ede01b0822e
S42yt <songoku42@outlook.de>
2025-06-09 02:27:59 +0300

    reverse changes

--- main.go
+++ main.go
@@ -83,7 +83,8 @@ func main() {
 				if analyzeImageForBite(img, redBox) {
 					fmt.Println("🐠 Biss-Signal erkannt! Klicke einmal...")
 					robotgo.Click("right")
-					
+					 time.Sleep(500 * time.Millisecond) // Removed delay
+
 				}
 			} else {
 				fmt.Println("🎣 Angel-Runde beendet. Werfe neu aus und warte...")
